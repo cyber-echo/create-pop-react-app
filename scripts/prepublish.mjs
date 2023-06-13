345054afada3361a6812cf9242b31d9c10c5a6f3
lin <linfengdoor@163.com>
2023-06-13 22:10:47 +0800

    init

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -2,6 +2,7 @@
 import 'zx/globals';
 
 await $`npm config set registry https://registry.npmjs.org`;
+await sleep(1000);
 await $`npm version patch`;
 
 const { version } = JSON.parse(await fs.readFile('./package.json'));
