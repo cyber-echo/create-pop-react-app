de7df83830089d1f50cc87233660b07fc0ac27c5
lin <linfengdoor@163.com>
2023-06-13 21:58:54 +0800

    init

---
+++ templates/default/ts/.eslintrc.cjs
@@ -0,0 +1,28 @@
+/**
+ * @eslint文档 https://zh-hans.eslint.org/docs/latest/use/configure/configuration-files
+ */
+
+const path = require('path');
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: path.join(__dirname, 'tsconfig.json')
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
+  }
+};
+
+module.exports = config;
